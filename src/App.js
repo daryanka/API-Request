9684d093b563632d2764e0c9371f4da0014a48c0
Daryan <daryanka@hotmail.co.uk>
2019-12-27 20:28:09 +0000

    get data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { useState } from 'react';
 import Axios from "axios";
+import _ from "lodash";
+import moment from "moment";
 
 const App = () => {
+  const [state , setState] = useState({});
 
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    const apiKey = "3500b52091a4b44d88536f2eb08f5304";
+    Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`).then(res => {
+      setState(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  };
 
   return (
     <div className="cont">
       <h1 className={"title"}>Weather API</h1>
 
       <div>
-        <p>Currently the weather would best be described as {"DESCRIPTION"}</p>
-        <p>With highs of {"HIGHS"}</p>
-        <p>And lows of {"LOWS"}</p>
-        <p>The sun will set at {"SUNRISE"}</p>
-        <p>And will rise at {"SUNSET"}</p>
+        {_.isEmpty(state) ? "Loading weather information": (
+          <>
+            <p>The temperature is currently {state.main.temp} but feels more like {state.main.feels_like}</p>
+            <p>Currently the weather would best be described as {state.weather[0].description}</p>
+            <p>With highs of {state.main.temp_max}</p>
+            <p>And lows of {state.main.temp_min}</p>
+            <p>The sun will set at {moment.unix(state.sys.sunrise).format("h:mm:ss a")}</p>
+            <p>And will rise at {moment.unix(state.sys.sunset).format("h:mm:ss a")}</p>
+          </>
+        )}
       </div>
 
       {/*<div className={"divider"}/>*/}
