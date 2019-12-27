5418932d60f5365ae6ee1cea8c519bd212ab25c9
Daryan <daryanka@hotmail.co.uk>
2019-12-27 20:45:29 +0000

    finished v1

--- src/App.js
+++ src/App.js
@@ -5,18 +5,55 @@ import moment from "moment";
 
 const App = () => {
   const [state , setState] = useState({});
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [cityName, setCityName] = useState("");
 
   React.useEffect(() => {
-    fetchData();
+    // fetchData();
   }, []);
 
   const fetchData = () => {
+    setError("");
     const apiKey = "3500b52091a4b44d88536f2eb08f5304";
-    Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`).then(res => {
-      setState(res.data)
+    setLoading(true);
+    Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`).then(res => {
+      setState(res.data);
+      setLoading(false);
+
     }).catch(err => {
+      setError("Unable to get weather information");
       console.log(err)
+      setLoading(false);
     })
+  };
+
+  const displayInformation = () => {
+    if (error) {
+      return <p className={"danger"}>{error}</p>
+    }
+
+    if (_.isEmpty(state) && loading) {
+      return <p>Loading weather information</p>
+    }
+
+    if (_.isEmpty(state)) {
+      return false
+    }
+
+    return (
+      <>
+        <p>The temperature is currently {state.main.temp}&#8451; but feels more like {state.main.feels_like}&#8451;</p>
+        <p>Currently the weather would best be described as {state.weather[0].description}</p>
+        <p>With highs of {state.main.temp_max}&#8451;</p>
+        <p>And lows of {state.main.temp_min}&#8451;</p>
+        <p>The sun will set at {moment.unix(state.sys.sunrise).format("h:mm:ss a")}</p>
+        <p>And will rise at {moment.unix(state.sys.sunset).format("h:mm:ss a")}</p>
+
+        <div className={"divider"}/>
+      </>
+    )
   };
 
   return (
@@ -24,21 +61,12 @@ const App = () => {
       <h1 className={"title"}>Weather API</h1>
 
       <div>
-        {_.isEmpty(state) ? "Loading weather information": (
-          <>
-            <p>The temperature is currently {state.main.temp} but feels more like {state.main.feels_like}</p>
-            <p>Currently the weather would best be described as {state.weather[0].description}</p>
-            <p>With highs of {state.main.temp_max}</p>
-            <p>And lows of {state.main.temp_min}</p>
-            <p>The sun will set at {moment.unix(state.sys.sunrise).format("h:mm:ss a")}</p>
-            <p>And will rise at {moment.unix(state.sys.sunset).format("h:mm:ss a")}</p>
-          </>
-        )}
+        {displayInformation()}
       </div>
 
-      {/*<div className={"divider"}/>*/}
-      {/*<p>Enter city name</p>*/}
-      {/*<input className={"input"}/>*/}
+      <p>Enter city name</p>
+      <input onChange={event => setCityName(event.target.value)} className={"input"}/>
+      <button className={"button"} onClick={fetchData}>Get Weather</button>
     </div>
   );
 }
